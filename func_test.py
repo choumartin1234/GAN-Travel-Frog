d1180da274548aef05a6ff7248cadeae53e7d92f
nonstopfor <zx-zhang18@mails.tsinghua.edu.cn>
2020-01-14 17:44:03 +0800

    loss

--- func_test.py
+++ func_test.py
@@ -1,6 +1,4 @@
 import tensorflow as tf
-
-a=tf.Variable(initial_value=[1,2,3,4])
-c=[1,2,3,4]
-b=reversed(c[:-1])
-print([_ for _ in b])
+from matplotlib import pyplot as plt
+plt.plot(range(5))
+plt.savefig('pic/1.png')
