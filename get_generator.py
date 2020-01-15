b9169a705d8e0c208d7111ee4fab4eabc38ed882
nonstopfor <zx-zhang18@mails.tsinghua.edu.cn>
2020-01-15 15:51:13 +0800

    saved

--- get_generator.py
+++ get_generator.py
@@ -1,11 +1,14 @@
 from pix2pix import *
-checkpoint_dir='checkpoints'
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+
+in_checkpoint_dir = 'checkpoints'
+out_checkpoint_dir = 'app_checkpoints'
+# checkpoint_prefix = os.path.join(in_checkpoint_dir, "ckpt")
 
 checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
                                  discriminator_optimizer=discriminator_optimizer,
                                  generator=generator,
                                  discriminator=discriminator)
-checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
-generator_checkpoint=tf.train.Checkpoint(generator=generator)
-generator_checkpoint.save(file_prefix=checkpoint_prefix)
+checkpoint.restore(tf.train.latest_checkpoint(in_checkpoint_dir))
+tf.saved_model.save(generator, out_checkpoint_dir)
+# generator_checkpoint=tf.train.Checkpoint(generator=generator)
+# generator_checkpoint.save(file_prefix=checkpoint_prefix)
